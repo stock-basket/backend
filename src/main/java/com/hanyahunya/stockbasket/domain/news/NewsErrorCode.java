b49a750df538c3429069c8e263febf462012dce8
ijaes <202230322@email.daelim.ac.kr>
2026-05-06 16:09:28 +0900

    add: 뉴스 서비스 추가, 기능 구현

--- src/main/java/com/hanyahunya/stockbasket/domain/news/NewsErrorCode.java
+++ src/main/java/com/hanyahunya/stockbasket/domain/news/NewsErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum NewsErrorCode implements ErrorCode {
-    NEWS_NOT_FOUND("NEWS_404", HttpStatus.NOT_FOUND, "존재하지 않는 뉴스입니다.");
+
+    NEWS_NOT_FOUND("NEWS_404", HttpStatus.NOT_FOUND, "존재하지 않는 뉴스입니다."),
+    INVALID_SENTIMENT_TYPE("NEWS_400", HttpStatus.BAD_REQUEST, "잘못된 감성 타입입니다.");
 
     private final String code;
     private final HttpStatus status;
